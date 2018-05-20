b58b3cbb4423e556c6f561b46c9a7294c54861ba
Erick Otenyo <otenyo.erick@gmail.com>
2018-05-20 21:55:19 +0300

    add speedometer

---
+++ src/Speedometer.js
@@ -0,0 +1,43 @@
+/* eslint-disable no-template-curly-in-string */
+import React from 'react';
+import ReactSpeedometer from 'react-d3-speedometer';
+
+// "top: 10px;right: 10px bottom: auto;left:auto"
+
+const Speedometer = ({ maxSpeed, currentSpeed }) => {
+  return (
+    <div
+      style={{
+        position: 'absolute',
+        zIndex: 10,
+        boxShadow: '0px 0px 10px #9ecaed',
+        border: '3px solid #9ecaed',
+        borderRadius: 5,
+        top: 10,
+        left: 'auto',
+        bottom: 'auto',
+        right: 10,
+        background: '#172236',
+      }}
+    >
+      <div style={{ padding: 10 }}>
+        <ReactSpeedometer
+          width={200}
+          height={150}
+          segments={10}
+          ringWidth={10}
+          value={currentSpeed}
+          needleTransitionDuration={100}
+          needleColor="#fff"
+          startColor="#33CC33"
+          endColor="#FF471A"
+          valueFormat="d"
+          maxValue={maxSpeed}
+          currentValueText="${value} km/h"
+        />
+      </div>
+    </div>
+  );
+};
+
+export default Speedometer;
